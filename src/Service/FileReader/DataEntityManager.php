ce920d360b2ce69208564401247291f1d156873e
emna.laameri <emnadsi@gmail.com>
2022-04-11 04:08:25 +0100

    feat: last check services

---
+++ src/Service/FileReader/DataEntityManager.php
@@ -0,0 +1,32 @@
+<?php
+// src/Service/FileReader/JsonFile.php
+namespace App\Service\FileReader;
+
+use App\Entity\Users;
+use Doctrine\ORM\EntityManager;
+
+class DataEntityManager
+{
+    public function save(EntityManager $entityManager, $datas, $isXml = null)
+    {
+        foreach($datas as $data){
+            $user = new Users;
+
+            if($isXml){
+                $data = (array) $data;
+            }
+
+            $user->setName($data['name']); 
+            $user->setHeight($data['height']); 
+            $user->setMass($data['mass']); 
+            $user->setHairColor($data['hair_color']); 
+            $user->setSkinColor($data['skin_color']); 
+            $user->setEyeColor($data['eye_color']); 
+            $user->setBirthYear($data['birth_year']); 
+            $user->setGender($data['gender']); 
+    
+            $entityManager->persist($user);
+            $entityManager->flush();
+        } 
+    }
+}
---
+++ tests/schirraTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class SchirraTest extends TestCase
+{
+    public function testDouble()
+    {
+        echo("hello");
+    }
+}
